9f2433fcc1177f8a3d9dd6b56cfb421b2d817d95
Matthew Bauer <bauer.s.matthew@gmail.com>
2018-01-05 22:52:11 -0800

    Rewrite/finish (somewhat tested)

--- nanopore/cb-np_count-isoforms.py
+++ nanopore/cb-np_count-isoforms.py
@@ -22,29 +22,43 @@ class BEDRecord:
         l = len(fields)
         self.nfields = l
         if l > 3: self.name = fields[3]
-        if l > 4: self.score = fields[4]
+        if l > 4: self.score = int(fields[4])
         if l > 5: self.strand = fields[5]
-        if l > 6: self.thickstart = fields[6]
-        if l > 7: self.thickend = fields[7]
+        if l > 6: self.thickstart = int(fields[6])
+        if l > 7: self.thickend = int(fields[7])
         if l > 8: self.color = fields[8]
         if l > 9:
             n_blocks = int(fields[9])
-            block_sizes = [int(x) for x in fields[10][:n_blocks]]
-            block_starts = [int(x) for x in fields[11][:n_blocks]]
+            block_sizes = list(map(int, fields[10].split(',')[:n_blocks]))
+            block_starts = list(map(int, fields[11].split(',')[:n_blocks]))
             self.blocks = []
             for i in range(n_blocks):
-                self.blocks.append((block_starts[i],
-                                    block_starts[i]+block_sizes[i]))
-        else:
-            self.blocks = [(self.start, self.end)]
+                self.blocks.append((self.start+block_starts[i],
+                                    self.start+block_starts[i]+block_sizes[i]))
 
-def tuple_overlap(t1, t2):
-    if t2[1] <= t1[0] or t2[0] >= t1[1]:
-        return 'none'
-    elif t2[0] >= t1[0] and t2[1] <= t1[1]:
-        return 'full'
-    else:
-        return 'partial'
+    def __str__(self):
+        """Store this record as a single BED line (no newline)."""
+        ret = '{}\t{}\t{}'.format(self.chrom, self.start, self.end)
+        l = self.nfields
+        if l > 3: ret += '\t{}'.format(self.name)
+        if l > 4: ret += '\t{}'.format(self.score)
+        if l > 5: ret += '\t{}'.format(self.strand)
+        if l > 6: ret += '\t{}'.format(self.thickstart)
+        if l > 7: ret += '\t{}'.format(self.thickend)
+        if l > 8: ret += '\t{}'.format(self.color)
+        if l > 9:
+            n_blocks = len(self.blocks)
+            block_starts = [b[0]-self.start for b in self.blocks]
+            block_sizes = [b[1]-b[0] for b in self.blocks]
+            ret += '\t{}\t{}\t{}'.format(
+                            n_blocks,
+                            ','.join(map(str, block_sizes)),
+                            ','.join(map(str, block_starts)))
+        return ret
+
+def get_overlap_interval(a, b):
+    """Get the number of bases overlap between two intervals."""
+    return max(0, min(a[1], b[1])-max(a[0], b[0]))
 
 def load_bed_file(path):
     """Load the BED file with the given path."""
@@ -103,46 +117,37 @@ if reference_path is None:
     separated = False
 _, reference = load_bed_file(reference_path)
 
-def init_count_structure():
-    """The counting structure is different depending on the reference type."""
-    global reference
-    ret = {}
-    for ref in reference:
-        if separated:
-            ret[ref.name] = 0
-        else:
-            ret[ref.name] = [0]*len(ref.blocks)
-    return ret
+features = {} # name -> interval
+for gene in reference:
+    if not separated:
+        for i, block in enumerate(gene.blocks):
+            name = '{}:{}'.format(gene.name, i+1)
+            if name in features:
+                raise RuntimeError('Feature names must be unique!')
+            features[name] = block
+    else:
+        if gene.name in features:
+            raise RuntimeError('Feature names must be unique!')
+        features[gene.name] = (gene.start, gene.end)
 
-# initialize count structure
-counts = init_count_structure()
-
-# go through each record
+tallies = {} # sorted tuple of names -> count
 for read in aligned_reads:
-    # count for this read
-    my_count = init_count_structure()
-    for block in read.blocks:
-        for ref in reference:
-            if separated and tuple_overlap(block, ref) != 'none':
-                my_count[ref.name] = 1
-            else:
-                for rbi, refblock in enumerate(ref.blocks):
-                    if tuple_overlap(block, refblock) != 'none':
-                        my_count[ref.name][rbi] = 1
-    # combine count for this read with total count
-    for name in my_count:
-        if separated:
-            counts[name] += my_count[name]
-        else:
-            for i in range(len(my_count[name])):
-                counts[name][i] += my_count[name][i]
+    found = []
+    for f_name in features:
+        for r_block in read.blocks:
+            if get_overlap_interval(r_block, features[f_name]) > 0:
+                if f_name not in found:
+                    found.append(f_name)
+    if found:
+        tup = tuple(sorted(found))
+        if tup not in tallies:
+            tallies[tup] = 0
+        tallies[tup] += 1
 
 # write results
-for name in counts:
-    if separated:
-        sys.stdout.write('{}\t{}\n'.format(name, counts[name]))
-    else:
-        for exon in range(len(counts[name])):
-            sys.stdout.write('{}:{}\t{}\n'.format(name, exon,
-                                                  counts[name][exon]))
+for pair in sorted(tallies.items(), key=lambda i: i[1], reverse=True):
+    tup = pair[0]
+    num = pair[1]
+    sys.stdout.write('{}\t{}\n'.format(','.join(tup), num))
+
 sys.exit(0)
